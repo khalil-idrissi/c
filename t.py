78801b8af16133af7feadbec7091259cd045510d
khalil-idrissi <khalil.idrissi1@uit.ac.ma>
2023-07-07 13:04:09 +0000

    dazdadeddsfs

--- t.py
+++ t.py
@@ -18,7 +18,7 @@ class Model:
         config.norm_type = "layernorm"
         self._tokenizer = AutoTokenizer.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True)
         self._model = AutoModelForCausalLM.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True, config=config)
-        self._model = deepspeed.init_inference(model=self._model, tensor_parallel={"tp_size":2}, dtype=torch.float16)
+        #self._model = deepspeed.init_inference(model=self._model, tensor_parallel={"tp_size":2}, dtype=torch.float16)
         self._model.to("cuda")
         #print(self._model.module)
         #print(dir(self._model))
@@ -29,7 +29,7 @@ class Model:
 
     def predict(self, model_input: Any, max_length=100, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1) -> str:
         x = self._tokenizer.encode(model_input, return_tensors='pt')
-        y = self._model.module.generate(x.cuda(), max_length=max_length, do_sample=do_sample, top_p=top_p, top_k=top_k, temperature=temperature, num_return_sequences=num_return_sequences, eos_token_id=self._tokenizer.eos_token_id)
+        y = self._model.generate(x.cuda(), max_length=max_length, do_sample=do_sample, top_p=top_p, top_k=top_k, temperature=temperature, num_return_sequences=num_return_sequences, eos_token_id=self._tokenizer.eos_token_id)
         generated_code = self._tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
         return generated_code
 
